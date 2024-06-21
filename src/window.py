f56a722966d1aacbf1b15c650268617e4f6dbe9a
Towster15 <105541074+Towster15@users.noreply.github.com>
2024-06-21 12:41:41 +0100

    Add change image functionality

--- src/window.py
+++ src/window.py
@@ -1,3 +1,5 @@
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Slot
 from PySide6.QtWidgets import (
     QWidget,
     QApplication,
@@ -7,7 +9,14 @@ from PySide6.QtWidgets import (
     QTableWidget,
     QGridLayout,
     QPushButton,
+    QMessageBox,
+    QFileDialog,
 )
+
+from PIL import Image, ImageQt
+
+from displayed_image import DisplayedImage
+from exceptions import FileFormatError
 
 
 class Window(QWidget):
@@ -20,6 +29,7 @@ class Window(QWidget):
 
         self.image_group = QGroupBox(title="Image")
         self.select_image_button = QPushButton("Select an image")
+        self.select_image_button.clicked.connect(self.__load_image)
         self.image_label = QLabel("No Image Loaded!")
         self.image_label.setScaledContents(True)
         self.image_group.layout = QVBoxLayout(self.image_group)
@@ -46,6 +56,29 @@ class Window(QWidget):
         self.layout.addWidget(self.core_info_group, 0, 1)
         self.layout.addWidget(self.os_info_group, 1, 1)
         self.layout.addWidget(self.all_exif_group, 0, 2, 2, 1)
+
+    def __change_image(self, image: Image) -> None:
+        pixmap = QPixmap(ImageQt.ImageQt(image))
+        self.image_label.setPixmap(pixmap)
+
+    @Slot()
+    def __load_image(self) -> None:
+        try:
+            # TODO: deal with string "" being returned when open dialog cancelled
+            self.displayed_image = DisplayedImage(
+                QFileDialog.getOpenFileName(
+                    self, "Open File", "/home", "Images (*.png *.xpm *.jpg)"
+                )[0]
+            )
+        except FileFormatError:
+            QMessageBox.warning(
+                self,
+                "Image Load Failed",
+                "Failed to load the image. Is it a valid image format?",
+            )
+        else:
+            self.__change_image(self.displayed_image.image)
+            pass
 
 
 if __name__ == "__main__":
